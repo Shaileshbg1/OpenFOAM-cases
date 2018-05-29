ba63f127673bf0d0fbc689d7f847c227ff7317bc
Shailesh <shailesh@niramai.com>
2018-05-29 12:06:11 +0530

    Added dataFile to gland and modified changeDictionaryDict to account for patch infusion

--- clean.sh
+++ clean.sh
@@ -11,11 +11,11 @@ rm -rf constant/polyMesh
 
 cd ../gland/
 find /home/shailesh/Niramai/Niramai_cases/with_subprocess/gland/constant -maxdepth 1 -mindepth 1 -not -name cellToRegion -not -name regionProperties -not -name toleranceDict -not -name triSurface -not -name gland -not -name muscle -not -name tissue -not -name tumor -exec rm -R {} \;
-find /home/shailesh/Niramai/Niramai_cases/with_subprocess/gland/system -maxdepth 1 -mindepth 1 -not -name blockMeshDict -not -name controlDict -not -name decomposeParDict -not -name fvSchemes -not -name fvSolution -not -name meshQualityDict -not -name snappyHexMeshDict -not -name snappyHexMeshDict.template -not -name topoSetDict -not -name topoSetDict.template -not -name gland -not -name tissue -not -name tumor -not -name muscle -not -name blockMeshDict.template -exec rm -R {} \;
+find /home/shailesh/Niramai/Niramai_cases/with_subprocess/gland/system -maxdepth 1 -mindepth 1 -not -name blockMeshDict -not -name controlDict -not -name decomposeParDict -not -name fvSchemes -not -name fvSolution -not -name meshQualityDict -not -name snappyHexMeshDict -not -name snappyHexMeshDict.template -not -name topoSetDict -not -name topoSetDict.template -not -name gland -not -name tissue -not -name tumor -not -name muscle -not -name blockMeshDict.template -not -name surfaces -exec rm -R {} \;
 rm -r 0/
 mkdir 0/
 
-find /home/shailesh/Niramai/Niramai_cases/with_subprocess/gland/ -maxdepth 1 -mindepth 1 -not -name T -not -name p -not -name 0 -not -name constant -not -name system -not -name changeDictionary.sh -exec rm -R {} \;
+find /home/shailesh/Niramai/Niramai_cases/with_subprocess/gland/ -maxdepth 1 -mindepth 1 -not -name T -not -name p -not -name 0 -not -name constant -not -name system -not -name changeDictionary.sh -not -name dataFile -exec rm -R {} \;
 rm -rf constant/gland/polyMesh
 rm -rf constant/muscle/polyMesh
 rm -rf constant/tissue/polyMesh
